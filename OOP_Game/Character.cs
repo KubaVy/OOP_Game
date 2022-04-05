be8b1f23aa698adb3f5cc599c93360b639e4c16f
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2022-04-05 10:18:51 +0200

    Add project files.

---
+++ OOP_Game/Character.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Game
+{
+    class Character
+    {
+        private int shield;
+        private int health;
+        private int damage;
+        public string Name { get;}
+        public int Health{ get => Health; }
+        public int Shield { get => shield; }
+        public int Damage { get => damage; }
+
+        public Character(string name)
+        {
+            Name = name;
+            health = 100;
+            shield = 50;
+            damage = 20;
+        }
+    
+        public void GetHit( int damage)
+        {
+            
+            shield -= damage;
+            if(shield < 0)
+            {
+                health -= shield;
+                shield = 0;
+            }
+            
+        }
+        public void Heal()
+        {
+            health += 15;
+        }
+        public override string ToString()
+        {
+            string text = "Name:" + "\n";
+            text += "Health:" + health + "\n";
+            text += "Shield:" + shield + "\n";
+            text += "Damage:" + damage + "\n";
+            return text;
+
+        }
+
+    }
+}
---
+++ OOP_Game/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Character character1;
+        Character character2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            character1 = new Character("Petr");
+            character2 = new Character("Pavel");
+            txt1.Text = character1.ToString();
+            txt2.Text = character2.ToString();
+        }
+
+        private void Button_Click_Attack(object sender, RoutedEventArgs e)
+        {
+            character2.GetHit(character1.Damage);
+            txt2.Text = character2.ToString();
+        }
+
+        private void Button_Click_heal(object sender, RoutedEventArgs e)
+        {
+            character1.Heal();
+            txt1.Text = character1.ToString();
+        }
+    }
+}
